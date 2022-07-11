333b79ec86ee1844a489509a30d01c1a69f38f39
Muhammad Fadlan <fasyafadlan7@gmail.com>
2022-07-11 11:50:27 +0700

    update app

--- app.py
+++ app.py
@@ -39,4 +39,4 @@ def my_form_post():
     return render_template('form.html', final=compound, text1=text_final,text2=dd['pos'],text5=dd['neg'],text4=compound,text3=dd['neu'])
 
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=5002, threaded=True)
+    app.run(debug=True, host="127.0.0.1", port=5000, threaded=True)
